76be015de7be3faede00506a5712bde81de73646
CamiloFalla <camilofallaa@hotmail.com>
2024-10-17 15:21:18 -0500

    juego ya operando sin save storage

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -23,23 +23,77 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 
 }
 
+const WINNER_COMBOS =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn]=useState(TURNS.X)
+  const [winner, setWinner]=useState(null)
 
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const checkEndGame = (boardToCheck) => {
+    return boardToCheck.every((square) => square !== null);
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+    //resetGameStorage()
+  }
   const updateBoard = (index) => {
+
+    if (board[index] || winner ) return
     const newBoard = [...board]
     newBoard[index] =turn
     setBoard(newBoard)
+
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+  
+    
+
+    const newWinner =checkWinner(newBoard)
+    if (newWinner){
+      setWinner(newWinner)
+      console.log(`¡Tenemos un ganador! Ganó: ${newWinner}`)
+    }else if (checkEndGame(newBoard)){
+      setWinner(false)
+      console.log('¡Es un empate')
+    }
   }
 
   return (
   <>
     <main className='board'>
       <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reiniciar Juego!</button>
       <section className='game'>
         {
           board.map((_, index) => {
@@ -64,13 +118,31 @@ function App() {
         </Square>
       </section>
 
+      {
+        winner != null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false ? 'Empate' : `Ganó`
+                }  
+              </h2>
+
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+
+              </header>
+              <footer>
+                <button onClick={resetGame}>Jugar de Nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )  
+      }
+
     </main>
   </>
   )
-  
-    
-  
-  
   
 }
 
